15503e6ff8e5b598cdd0ba36b19287ca1530097f
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-06-05 10:30:56 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/cmjd/lostandfoundappbackend/LostAndFoundAppBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd.lostandfoundappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostAndFoundAppBackendApplication.class, args);
+    }
+
+}
